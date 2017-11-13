70f03b5970b1b0421dfc48d15a636b7f803e9bba
Im Khem <im.khem@gmail.com>
2017-11-13 00:56:41 -0800

    auth setup

--- app.js
+++ app.js
@@ -1,7 +1,36 @@
 const express = require('express');
+const exphbs  = require('express-handlebars');
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+// Passport
+require('./config/passport')(passport);
+
+// Routes
+const auth = require('./routes/auth');
+
 const app = express();
 
-const port = 3000;
+// Connect to mongoose
+
+// Handlebars Middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// Index Route
+app.get('/', (req,res)=>{
+  res.render('index');
+})
+
+// About Route
+app.get('/about', (req,res)=>{
+  res.render('about');
+})
+
+// Use Routes
+app.use('/auth', auth);
+
+const port = process.env.PORT || 3000;
 
 app.listen(port,()=>{
   console.log(`Server started on ${port}.`);
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: 'mongodb://im:password@ds259305.mlab.com:59305/atlas-dev',
+  googleClientID: '793371488604-jrh4ajo14qe6m5168duk70da7ras6com.apps.googleusercontent.com',
+  googleClientSecret: 'LCSLRv5O_zO_Z6rA5kN0Bdm5'
+}
